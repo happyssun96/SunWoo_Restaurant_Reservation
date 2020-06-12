28d9e3ff04bb3c44372e358f2d105322f3a9f970
sunwoo <lsw96@naver.com>
2020-06-12 10:34:17 +0900

    first commit

---
+++ lecpro/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+import ouruser.views
+import ouruser.urls
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', ouruser.views.home, name = 'index'),
+    path('user/', include('ouruser.urls')),
+    path('accounts/', include('allauth.urls')),
+]
